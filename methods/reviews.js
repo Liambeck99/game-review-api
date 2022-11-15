84234b4abc7c291e76b0e1723a090ffd1c2e5e39
Liam Beckingham <57397563+Liambeck99@users.noreply.github.com>
2022-11-15 12:15:17 +0000

    update query string in selectReviews

--- methods/reviews.js
+++ methods/reviews.js
@@ -16,14 +16,7 @@ exports.selectReviews = () => {
   FROM reviews
   LEFT JOIN comments ON comments.review_id = reviews.review_id
   GROUP BY
-    reviews.category,
-    reviews.created_at,
-    reviews.designer,
-    reviews.owner,
-    reviews.review_id,
-    reviews.review_img_url,
-    reviews.title,
-    reviews.votes
+    reviews.review_id
   ORDER BY reviews.created_at DESC
   ;`;
 
