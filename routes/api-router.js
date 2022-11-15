01b8e544dbceb1fbf7023b90edf0384d62418e8a
Liam Beckingham <57397563+Liambeck99@users.noreply.github.com>
2022-11-15 10:42:21 +0000

    Update api-router.js

--- routes/api-router.js
+++ routes/api-router.js
@@ -2,7 +2,7 @@ const categoriesRouter = require("./categories-router");
 const apiRouter = require("express").Router();
 
 apiRouter.route("/").get((req, res) => {
-  message = "ALL OK from GET /api";
+  const message = "ALL OK from GET /api";
   res.status(200).send({ message });
 });
 
